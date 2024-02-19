1c18444e3fd728a1fc6f29594937421bcbfef61f
Pawan Kumar Vishwakarma <pawan07052001@gmail.com>
2024-02-20 00:45:06 +0530

    Model Data using ViewData

---
+++ ModelDataUsingViewData/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelDataUsingViewData.Models;
+using System.Diagnostics;
+
+namespace ModelDataUsingViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //Employee emp = new Employee()
+            //{
+            //    EmpId = 1,
+            //    EmpName = "Shubham",
+            //    Designation = "Developer",
+            //    Salary = 25000
+
+            //};
+            //ViewData["Employee"] = emp;
+            //ViewBag.Employee = emp;
+            //TempData["Employee"] = emp;
+
+            List<Employee> employees = new List<Employee>()
+            { 
+                new Employee {EmpId = 101,EmpName = "Pawan",Designation = "Devloper",Salary = 25000},
+                new Employee {EmpId = 102,EmpName = "Raj",Designation = "SDE II",Salary = 30000},
+                new Employee {EmpId = 103,EmpName = "Shubham",Designation = "Tester",Salary = 20000},
+                new Employee {EmpId = 104,EmpName = "Rajesh",Designation = "DBA",Salary = 25000},
+                new Employee {EmpId = 105,EmpName = "Nikhil",Designation = "Data Analyst",Salary = 21000}
+
+            };
+            ViewData["Employees"] = employees;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ModelDataUsingViewData/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace ModelDataUsingViewData.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+    }
+}
