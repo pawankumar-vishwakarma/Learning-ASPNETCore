6072e3a712577b6595f43325bb3eca9e4bfb15e2
Pawan Kumar Vishwakarma <pawan07052001@gmail.com>
2024-02-20 00:51:21 +0530

    Strongly Typed View

---
+++ StronglyTypedViewASPCore/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StronglyTypedViewASPCore.Models;
+using System.Diagnostics;
+
+namespace StronglyTypedViewASPCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Employee emp = new Employee()
+            {
+                EmpId = 1,
+                EmpName = "Pawan",
+                Salary  = 25000
+            };
+            return View(emp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StronglyTypedViewASPCore/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace StronglyTypedViewASPCore.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int Salary { get; set; }
+    }
+}
