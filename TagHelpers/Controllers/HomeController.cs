c0c398728bfc9db1eea55587d20b20dea2a9b069
Pawan Kumar Vishwakarma <pawan07052001@gmail.com>
2024-02-22 01:45:46 +0530

    Tag Helpers

---
+++ TagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TagHelpers.Models;
+
+namespace TagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Index(Employee emp)
+        {
+            return "Name: "+ emp.Name+" Gender: "+emp.Gender+" Age: "+emp.Age+" Designation: "+emp.Designation+" Salary: "+emp.Salary+" Married: "+emp.Married+" Description: "+emp.Description;
+        }
+
+        //public IActionResult Contact()
+        //{
+        //    return View();
+        //}
+
+        public int Edit(int id)
+        {
+            return id;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TagHelpers/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace TagHelpers.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public string Designation { get; set;}
+        public int Salary { get; set; }
+        public string Married { get; set; }
+        public string Description { get; set; }
+    }
+
+    public enum Gender
+    {
+        MALE, FEMALE
+    }
+}
