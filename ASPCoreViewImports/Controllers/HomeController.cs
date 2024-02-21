fb18c44515f493a6280e0e5f1844f5ab458ec601
Pawan Kumar Vishwakarma <pawan07052001@gmail.com>
2024-02-21 15:15:19 +0530

    View Imports

---
+++ ASPCoreViewImports/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASPCoreViewImports.Models;
+
+namespace ASPCoreViewImports.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student {Id= 1, Name="Pawan",Standard=12},
+                new Student {Id=2, Name="Dinesh",Standard=11}
+            };
+            return View(students);
+        }
+        public IActionResult About()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student {Id= 1, Name="Pawan",Standard=12},
+                new Student {Id=2, Name="Dinesh",Standard=11}
+            };
+            return View(students);
+        }
+        public IActionResult Contact()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student {Id= 1, Name="Pawan",Standard=12},
+                new Student {Id=2, Name="Dinesh",Standard=11}
+            };
+            return View(students);
+        }
+
+    }
+}
---
+++ ASPCoreViewImports/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPCoreViewImports.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Standard { get; set; }
+    }
+}
