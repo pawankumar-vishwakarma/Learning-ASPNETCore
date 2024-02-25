1b8ff1f70264a4d4be24a8f7316ac8893f2018a5
Pawan Kumar Vishwakarma <pawan07052001@gmail.com>
2024-02-25 23:43:44 +0530

    ASPNETCore Web API

---
+++ ASPCoreWebAPI/Controllers/FruitsAPIController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsAPIController : ControllerBase
+    {
+        public List<string> Fruits = new List<string>()
+        {
+            "Apple",
+            "Banana",
+            "Grapes",
+            "Mango"
+        };
+
+        [HttpGet]
+        public List<string> GetFruits()
+        {
+            return Fruits;
+        }
+
+        [HttpGet("{id}")]
+        public string GetFruitsById(int id)
+        {
+            return Fruits.ElementAt(id);
+        }
+    }
+}
